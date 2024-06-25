1ee45965c875936287052a0069bd930234aaf2e5
TahaNaeem1995 <160178535+TahaNaeem1995@users.noreply.github.com>
2024-06-25 15:56:39 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+// for(let a = 100; a <= 1000; a++){
+//     console.log(" Pakistan Zindabad");
+//     console.log(a);
+Object.defineProperty(exports, "__esModule", { value: true });
+// }
+// let a = 1; a <= 50; a++
+// console.log("Pakistan Zindabad");
+// console.log(a);
+for (let a = 1; a <= 100; ++a) {
+    console.log(" Pakistan Zindabad");
+    console.log(" Pakistan Zindabad");
+    console.log("taha naeem");
+    console.log("my name is sania");
+    console.log("my name is hira");
+    console.log("my name is sona");
+}
---
+++ main.ts
@@ -0,0 +1,22 @@
+// for(let a = 100; a <= 1000; a++){
+//     console.log(" Pakistan Zindabad");
+//     console.log(a);
+
+// }
+// let a = 1; a <= 50; a++
+
+// console.log("Pakistan Zindabad");
+// console.log(a);
+
+
+for (let a = 1; a <= 100; ++a) {
+    console.log(" Pakistan Zindabad");
+    console.log(" Pakistan Zindabad");
+    console.log("taha naeem");
+    console.log("my name is sania");
+    console.log("my name is hira");
+    console.log("my name is sona");
+
+}
+
+
